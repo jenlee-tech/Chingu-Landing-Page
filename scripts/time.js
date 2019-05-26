8557765d99cccd2396bcd268d094280542c83aac
Jennifer Lee <jenlee.tech@gmail.com>
2019-05-26 00:32:27 -0400

    added links for nav bar and scripts directory and inserted time in HTML

---
+++ scripts/time.js
@@ -0,0 +1,28 @@
+window.onload =
+    function GetClock() {
+        let d = new Date();
+        let nhour = d.getHours();
+        let nmin = d.getMinutes();
+        let ap;
+        let clocktext;
+        if (nhour == 0) {
+            ap = " AM";
+            nhour = 12;
+        } else if (nhour < 12) {
+            ap = " AM";
+        } else if (nhour == 12) {
+            ap = " PM";
+        } else if (nhour > 12) {
+            ap = " PM";
+            nhour -= 12;
+        };
+
+        if (nmin <= 9) nmin = "0" + nmin;
+
+        clocktext = "" + nhour + ":" + nmin + ap + "";
+        document.getElementById("clockbox").innerHTML = clocktext;
+
+    };
+
+GetClock()
+setInterval(GetClock, 1000);
