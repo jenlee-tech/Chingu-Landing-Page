1b563d34679fc9951db3ade993dddf5d36113ee2
Jennifer Lee <jenlee.tech@gmail.com>
2019-05-27 14:16:44 -0400

    added another script with date stamp

--- scripts/time.js
+++ scripts/time.js
@@ -1,34 +1,42 @@
-    function GetClock() {
-        let d = new Date();
-        let nhour = d.getHours();
-        let nmin = d.getMinutes();
-        let ap;
-        let clocktext;
-        if (nhour == 0) {
-            ap = " AM";
-            nhour = 12;
-        } else if (nhour < 12) {
-            ap = " AM";
-        } else if (nhour == 12) {
-            ap = " PM";
-        } else if (nhour > 12) {
-            ap = " PM";
-            nhour -= 12;
-        };
+function GetClock() {
+    let d = new Date();
+    let nhour = d.getHours();
+    let nmin = d.getMinutes();
+    let ap;
+    let clocktext;
+    let month = d.toLocaleDateString('en-us', {
+        month: 'long'
+    });
 
 
-        if (nmin <= 9) nmin = "0" + nmin;
-
-
-        clocktext = "" + nhour + ":" + nmin + "";
-
-
-        document.getElementById("timestamp").innerHTML = clocktext;
-        document.getElementById("ap-text").innerHTML = ap;
-
-
-
+    let datedisplay;
+    if (nhour == 0) {
+        ap = " AM";
+        nhour = 12;
+    } else if (nhour < 12) {
+        ap = " AM";
+    } else if (nhour == 12) {
+        ap = " PM";
+    } else if (nhour > 12) {
+        ap = " PM";
+        nhour -= 12;
     };
 
-    GetClock()
-    setInterval(GetClock, 1000);
+    if (nmin <= 9) nmin = "0" + nmin;
+    clocktext = "" + nhour + ":" + nmin + "";
+    document.getElementById("timestamp").innerHTML = clocktext;
+    document.getElementById("ap-text").innerHTML = ap;
+
+
+
+};
+
+
+
+
+
+
+
+GetClock()
+setInterval(GetClock, 1000);
+//setInterval(dateStamp, 1000);
